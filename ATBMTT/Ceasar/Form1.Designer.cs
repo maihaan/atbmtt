565fbb211ea27d6cd294c79fb2c05c9026117753
mai ha an <maihaan24@gmail.com>
2018-08-16 14:24:55 +0700

    Mã nguồn các ví dụ
    Mã hóa Ceasar

---
+++ ATBMTT/Ceasar/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Ceasar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDuLieuCanMaHoa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDuLieuMaHoaDuoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDuLieuGiaiMaDuoc = new System.Windows.Forms.TextBox();
+            this.btMaHoa = new System.Windows.Forms.Button();
+            this.btGiaiMa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbDuLieuCanMaHoa
+            // 
+            this.tbDuLieuCanMaHoa.Location = new System.Drawing.Point(15, 25);
+            this.tbDuLieuCanMaHoa.Multiline = true;
+            this.tbDuLieuCanMaHoa.Name = "tbDuLieuCanMaHoa";
+            this.tbDuLieuCanMaHoa.Size = new System.Drawing.Size(552, 56);
+            this.tbDuLieuCanMaHoa.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dữ liệu cần mã hóa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dữ liệu mã hóa được";
+            // 
+            // tbDuLieuMaHoaDuoc
+            // 
+            this.tbDuLieuMaHoaDuoc.Location = new System.Drawing.Point(15, 141);
+            this.tbDuLieuMaHoaDuoc.Multiline = true;
+            this.tbDuLieuMaHoaDuoc.Name = "tbDuLieuMaHoaDuoc";
+            this.tbDuLieuMaHoaDuoc.Size = new System.Drawing.Size(552, 56);
+            this.tbDuLieuMaHoaDuoc.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dữ liệu giải mã được";
+            // 
+            // tbDuLieuGiaiMaDuoc
+            // 
+            this.tbDuLieuGiaiMaDuoc.Location = new System.Drawing.Point(14, 260);
+            this.tbDuLieuGiaiMaDuoc.Multiline = true;
+            this.tbDuLieuGiaiMaDuoc.Name = "tbDuLieuGiaiMaDuoc";
+            this.tbDuLieuGiaiMaDuoc.Size = new System.Drawing.Size(552, 56);
+            this.tbDuLieuGiaiMaDuoc.TabIndex = 5;
+            // 
+            // btMaHoa
+            // 
+            this.btMaHoa.Location = new System.Drawing.Point(15, 88);
+            this.btMaHoa.Name = "btMaHoa";
+            this.btMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btMaHoa.TabIndex = 6;
+            this.btMaHoa.Text = "Mã hóa";
+            this.btMaHoa.UseVisualStyleBackColor = true;
+            this.btMaHoa.Click += new System.EventHandler(this.btMaHoa_Click);
+            // 
+            // btGiaiMa
+            // 
+            this.btGiaiMa.Location = new System.Drawing.Point(14, 203);
+            this.btGiaiMa.Name = "btGiaiMa";
+            this.btGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btGiaiMa.TabIndex = 7;
+            this.btGiaiMa.Text = "Giải mã";
+            this.btGiaiMa.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 330);
+            this.Controls.Add(this.btGiaiMa);
+            this.Controls.Add(this.btMaHoa);
+            this.Controls.Add(this.tbDuLieuGiaiMaDuoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDuLieuMaHoaDuoc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDuLieuCanMaHoa);
+            this.Name = "Form1";
+            this.Text = "Mã hóa Ceasar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDuLieuCanMaHoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDuLieuMaHoaDuoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDuLieuGiaiMaDuoc;
+        private System.Windows.Forms.Button btMaHoa;
+        private System.Windows.Forms.Button btGiaiMa;
+    }
+}
+
---
+++ ATBMTT/Ceasar/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ceasar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Bang chu cai
+        char[] bangChuCai = new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y','z', ' ', '1', '2', '3', '4',
+            '5', '6', '7', '8', '9', '0', '@', '#', '$', '%', '^',
+            '&', '*', '(', ')','_','-', '=', '+','>', '<', '?','/',
+            '\\','{', '}','[',']','ê', 'â', 'ơ', 'ư','ô','ă'
+        };
+
+        int k = 5;
+
+        private void btMaHoa_Click(object sender, EventArgs e)
+        {
+            // Kiem tra du lieu nhap vao
+            if(tbDuLieuCanMaHoa.Text.Length == 0)
+            {
+                MessageBox.Show("Bạn chưa nhập dữ liệu cần mã hóa");
+                return;
+            }
+
+            // Ma hoa
+            String duLieuCanMaHoa = tbDuLieuCanMaHoa.Text;
+            String ketQuaMaHoa = "";
+
+            // - Duyet tung ky tu trong doan van ban
+
+            for (int i = 0; i < duLieuCanMaHoa.Length; i++)
+            {
+                char kyTuCanMa = duLieuCanMaHoa[i];
+                int viTriKyTuCanMa = -1;
+                
+                // -- Tim vi tri cua ky tu trong bang chu cai
+                for(int j = 0; j < bangChuCai.Length; j++)
+                {
+                    if(bangChuCai[j].Equals(kyTuCanMa))
+                    {
+                        viTriKyTuCanMa = j;
+                        break;
+                    }
+                }
+
+                // -- Kiem tra xem ky tu can ma co trong bang chu cai hay khong va ma hoa
+                if(viTriKyTuCanMa == -1)
+                {
+                    ketQuaMaHoa += kyTuCanMa.ToString();
+                }
+                else
+                {
+                    int viTriMoi = (viTriKyTuCanMa + k) % bangChuCai.Length;
+                    ketQuaMaHoa += bangChuCai[viTriMoi].ToString();
+                }
+            }
+
+            // Hien thi ket qua ra giao dien
+            tbDuLieuMaHoaDuoc.Text = ketQuaMaHoa;
+        }
+    }
+}
