49805d3356daec5eb0322595764409662ab88dae
mai ha an <maihaan24@gmail.com>
2018-08-30 14:01:53 +0700

    Cập nhật File Encrypt
    Cập nhật File Encrypt

---
+++ ATBMTT/FileEncrypt/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FileEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDuongDan = new System.Windows.Forms.TextBox();
+            this.btTim = new System.Windows.Forms.Button();
+            this.rbMaHoa = new System.Windows.Forms.RadioButton();
+            this.rbGiaiMa = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMatKhau = new System.Windows.Forms.TextBox();
+            this.btThucHien = new System.Windows.Forms.Button();
+            this.pbTienTrinh = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn tệp tin";
+            // 
+            // tbDuongDan
+            // 
+            this.tbDuongDan.Location = new System.Drawing.Point(16, 30);
+            this.tbDuongDan.Name = "tbDuongDan";
+            this.tbDuongDan.Size = new System.Drawing.Size(284, 20);
+            this.tbDuongDan.TabIndex = 1;
+            // 
+            // btTim
+            // 
+            this.btTim.Location = new System.Drawing.Point(306, 28);
+            this.btTim.Name = "btTim";
+            this.btTim.Size = new System.Drawing.Size(75, 23);
+            this.btTim.TabIndex = 2;
+            this.btTim.Text = "Tìm";
+            this.btTim.UseVisualStyleBackColor = true;
+            this.btTim.Click += new System.EventHandler(this.btTim_Click);
+            // 
+            // rbMaHoa
+            // 
+            this.rbMaHoa.AutoSize = true;
+            this.rbMaHoa.Location = new System.Drawing.Point(13, 57);
+            this.rbMaHoa.Name = "rbMaHoa";
+            this.rbMaHoa.Size = new System.Drawing.Size(61, 17);
+            this.rbMaHoa.TabIndex = 3;
+            this.rbMaHoa.TabStop = true;
+            this.rbMaHoa.Text = "Mã hóa";
+            this.rbMaHoa.UseVisualStyleBackColor = true;
+            this.rbMaHoa.CheckedChanged += new System.EventHandler(this.rbMaHoa_CheckedChanged);
+            // 
+            // rbGiaiMa
+            // 
+            this.rbGiaiMa.AutoSize = true;
+            this.rbGiaiMa.Location = new System.Drawing.Point(80, 57);
+            this.rbGiaiMa.Name = "rbGiaiMa";
+            this.rbGiaiMa.Size = new System.Drawing.Size(60, 17);
+            this.rbGiaiMa.TabIndex = 4;
+            this.rbGiaiMa.TabStop = true;
+            this.rbGiaiMa.Text = "Giải mã";
+            this.rbGiaiMa.UseVisualStyleBackColor = true;
+            this.rbGiaiMa.CheckedChanged += new System.EventHandler(this.rbGiaiMa_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // tbMatKhau
+            // 
+            this.tbMatKhau.Location = new System.Drawing.Point(71, 78);
+            this.tbMatKhau.Name = "tbMatKhau";
+            this.tbMatKhau.PasswordChar = '*';
+            this.tbMatKhau.Size = new System.Drawing.Size(172, 20);
+            this.tbMatKhau.TabIndex = 6;
+            // 
+            // btThucHien
+            // 
+            this.btThucHien.BackColor = System.Drawing.SystemColors.Info;
+            this.btThucHien.Location = new System.Drawing.Point(12, 114);
+            this.btThucHien.Name = "btThucHien";
+            this.btThucHien.Size = new System.Drawing.Size(369, 32);
+            this.btThucHien.TabIndex = 7;
+            this.btThucHien.Text = "Thực hiện";
+            this.btThucHien.UseVisualStyleBackColor = false;
+            this.btThucHien.Click += new System.EventHandler(this.btThucHien_Click);
+            // 
+            // pbTienTrinh
+            // 
+            this.pbTienTrinh.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pbTienTrinh.Location = new System.Drawing.Point(0, 167);
+            this.pbTienTrinh.Name = "pbTienTrinh";
+            this.pbTienTrinh.Size = new System.Drawing.Size(394, 23);
+            this.pbTienTrinh.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 190);
+            this.Controls.Add(this.pbTienTrinh);
+            this.Controls.Add(this.btThucHien);
+            this.Controls.Add(this.tbMatKhau);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rbGiaiMa);
+            this.Controls.Add(this.rbMaHoa);
+            this.Controls.Add(this.btTim);
+            this.Controls.Add(this.tbDuongDan);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mã hóa tệp tin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDuongDan;
+        private System.Windows.Forms.Button btTim;
+        private System.Windows.Forms.RadioButton rbMaHoa;
+        private System.Windows.Forms.RadioButton rbGiaiMa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMatKhau;
+        private System.Windows.Forms.Button btThucHien;
+        private System.Windows.Forms.ProgressBar pbTienTrinh;
+    }
+}
+
---
+++ ATBMTT/FileEncrypt/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileEncrypt
+{
+    public partial class Form1 : Form
+    {
+        byte[] bangChuCai;
+        byte[,] bangTra;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btTim_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if(od.ShowDialog() == DialogResult.OK)
+            {
+                tbDuongDan.Text = od.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbMaHoa.Checked = true;
+            // Khoi tao bang chu cai
+            bangChuCai = new byte[256];
+            for (int i = 0; i < 256; i++)
+                bangChuCai[i] = Convert.ToByte(i);
+
+            // Khoi tao bang tra
+            bangTra = new byte[256, 256];
+            for(int i = 0; i < 256; i++)
+                for(int j = 0; j < 256; j++)
+                {
+                    bangTra[i, j] = bangChuCai[(i + j) % 256];
+                }
+        }
+
+        private void rbMaHoa_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbMaHoa.Checked)
+            {
+                rbGiaiMa.Checked = false;
+            }
+        }
+
+        private void rbGiaiMa_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbGiaiMa.Checked)
+            {
+                rbMaHoa.Checked = false;
+            }
+        }
+
+        private void btThucHien_Click(object sender, EventArgs e)
+        {
+            // Kiem tra xem tep tin nguoi dung chon co ton tai hay ko
+            if(!File.Exists(tbDuongDan.Text))
+            {
+                MessageBox.Show("Tệp tin không tồn tại. Bạn hãy chọn lại tệp tin.");
+                return;
+            }
+
+            // Kiem tra xem nguoi dung da nhap mat khau hay chua
+            if(tbMatKhau.Text.Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập mật khẩu.");
+                return;
+            }
+
+            // Chuan bi du lieu de ma hoa hoac giai ma
+            try
+            {
+                // Doc noi dung tep tin
+                byte[] noiDungTepTin = File.ReadAllBytes(tbDuongDan.Text);
+                // Sinh khoa tu mat khau
+                byte[] matKhauByte = Encoding.ASCII.GetBytes(tbMatKhau.Text);
+                byte[] khoa = new byte[noiDungTepTin.Length];
+                for (int i = 0; i < khoa.Length; i++)
+                    khoa[i] = matKhauByte[i % matKhauByte.Length];
+
+                // Thuc hien cong viec
+                byte[] ketQua = new byte[noiDungTepTin.Length];
+                if(rbMaHoa.Checked)
+                {
+                    // Ma hoa tep tin
+                    for(int i = 0; i < noiDungTepTin.Length; i++)
+                    {
+                        byte kyTuCanMa = noiDungTepTin[i];
+                        byte kyTuKhoa = khoa[i];
+                        int viTriKyTuCanMa = -1, viTriKyTuKhoa = -1;
+                        for(int j = 0; j < 256; j++)
+                            if(bangChuCai[j] == kyTuCanMa)
+                            {
+                                viTriKyTuCanMa = j;
+                                break;
+                            }
+                        for(int j = 0; j < 256; j++)
+                            if(bangChuCai[j] == kyTuKhoa)
+                            {
+                                viTriKyTuKhoa = j;
+                                break;
+                            }
+                        ketQua[i] = bangTra[viTriKyTuKhoa, viTriKyTuCanMa];
+                    }
+                    // Luu ket qua vao tep tin
+                    String ext = Path.GetExtension(tbDuongDan.Text);
+                    SaveFileDialog sd = new SaveFileDialog();
+                    sd.Filter = "File type (*" + ext + ") | *" + ext;
+                    if(sd.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllBytes(sd.FileName, ketQua);
+                    }
+                }
+                else
+                {
+                    // Giai ma tep tin
+                    for (int i = 0; i < noiDungTepTin.Length; i++)
+                    {
+                        byte kyTuCanMa = noiDungTepTin[i];
+                        byte kyTuKhoa = khoa[i];
+                        int viTriKyTuCanMa = -1, viTriKyTuKhoa = -1;                        
+                        for (int j = 0; j < 256; j++)
+                            if (bangChuCai[j] == kyTuKhoa)
+                            {
+                                viTriKyTuKhoa = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (bangTra[viTriKyTuKhoa, j] == kyTuCanMa)
+                            {
+                                viTriKyTuCanMa = j;
+                                break;
+                            }
+                        ketQua[i] = bangChuCai[viTriKyTuCanMa];
+                    }
+                    // Luu ket qua vao tep tin
+                    String ext = Path.GetExtension(tbDuongDan.Text);
+                    SaveFileDialog sd = new SaveFileDialog();
+                    sd.Filter = "File type (*" + ext + ") | *" + ext;
+                    if (sd.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllBytes(sd.FileName, ketQua);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Tệp tin đang được sử dụng bởi chương trình khác, hãy đóng chương trình đang sử dụng tệp tin và thử lại.");
+                return;
+            }
+        }
+    }
+}
